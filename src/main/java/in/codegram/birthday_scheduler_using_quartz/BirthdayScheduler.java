fb80f70e4595e918c81c9ccceb83eae749f0ac2f
ranjeet <ranjeetrai.gkp@gmail.com>
2024-03-17 11:06:30 +0530

    scheduler demo project for scheduling job

---
+++ src/main/java/in/codegram/birthday_scheduler_using_quartz/BirthdayScheduler.java
@@ -0,0 +1,13 @@
+package in.codegram.birthday_scheduler_using_quartz;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BirthdayScheduler {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BirthdayScheduler.class, args);
+	}
+
+}
---
+++ src/test/java/in/codegram/user_module/UserManagementServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package in.codegram.user_module;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UserManagementServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
